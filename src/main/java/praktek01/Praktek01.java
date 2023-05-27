67339f9444d9ea995b6bae46424b4286b2e2eb40
riska <riskafawn@gmail.com>
2023-05-27 15:22:24 +0700

    Initial commit

---
+++ src/main/java/praktek01/Praktek01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package praktek01;
+
+/**
+ *
+ * @author Windows 11
+ */
+public class Praktek01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
